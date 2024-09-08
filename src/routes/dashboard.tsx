50168d49453c9efb6285ad243672e4aebbff34b2
Chii Yuen <chii.yuen@hotmail.com>
2024-09-08 16:06:18 +0800

    Improve table loading

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -10,26 +10,44 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Dashboard() {
   const { connected, publicKey } = useUnifiedWallet();
   const { connection } = useConnection();
   const [balance, setBalance] = useState<number | null>(null);
   const [transactions, setTransactions] = useState<ConfirmedSignatureInfo[]>([]);
+  const [txLimit, setTxLimit] = useState<number>(15);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchData() {
       if (publicKey) {
         setBalance(await connection.getBalance(publicKey) / LAMPORTS_PER_SOL);
-
-        const sigs = await connection.getSignaturesForAddress(publicKey);
-
-        setTransactions(sigs);
       }
     }
 
     fetchData()
   }, [connection, publicKey]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (publicKey) {
+        setIsFetching(true);
+
+        const txs = await connection.getSignaturesForAddress(publicKey, {
+          limit: txLimit,
+        });
+
+        setTransactions(txs)
+        setIsFetching(false);
+      }
+    };
+
+    fetchData();
+  }, [connection, publicKey, txLimit]);
 
   return (
     <main className="flex flex-col gap-y-4 items-center">
@@ -40,28 +58,45 @@ export default function Dashboard() {
           <h2>Transaction History</h2>
           <Table>
             <TableHeader>
-              <TableRow>
-                <TableHead className="w-[300px]">Signature</TableHead>
-                <TableHead className="w-[125px]">Block</TableHead>
-                <TableHead className="w-[125px">Slot</TableHead>
-                <TableHead className="text-right">Status</TableHead>
+              <TableRow className="justify-end">
+                <TableHead className="w-full min-w-[150px]">Signature</TableHead>
+                <TableHead className="min-w-[125px]">Block</TableHead>
+                <TableHead className="min-w-[125px]">Slot</TableHead>
+                <TableHead className="text-right min-w-[100px]">Status</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {transactions.map((transaction) => (
-                <TableRow key={transaction.signature} className="">
-                  <TableCell className="w-full">{transaction.signature}</TableCell>
-                  <TableCell className="min-w-[100px]">{transaction.blockTime}</TableCell>
-                  <TableCell className="min-w-[100px]">{transaction.slot}</TableCell>
+              {transactions.length ? transactions.map((transaction) => (
+                <TableRow key={transaction.signature} className="overflow-hidden justify-end">
+                  <TableCell className="w-full min-w-[150px] text-ellipsis">{transaction.signature}</TableCell>
+                  <TableCell className="min-w-[125px]">{transaction.blockTime}</TableCell>
+                  <TableCell className="min-w-[125px]">{transaction.slot}</TableCell>
                   <TableCell className={`text-right min-w-[100px] font-bold ${transaction.err ? "text-red-400" : "text-green-400"}`}>{transaction.err ? "Error" : "Success"}</TableCell>
                 </TableRow>
-              ))}
+              )) :
+                (<>
+                  {Array.from({ length: 5 }).map((_) => (
+                    <TableRow className="overflow-hidden justify-end">
+                      <TableCell><Skeleton className="w-full h-[20px]"></Skeleton></TableCell>
+                      <TableCell><Skeleton className="w-full h-[20px]"></Skeleton></TableCell>
+                      <TableCell><Skeleton className="w-full h-[20px]"></Skeleton></TableCell>
+                      <TableCell><Skeleton className="w-full h-[20px]"></Skeleton></TableCell>
+                    </TableRow>
+                  ))}
+                </>)}
             </TableBody>
           </Table>
+          {Boolean(transactions.length) &&
+            <Button disabled={isFetching} onClick={() => setTxLimit(p => p + 15)}>
+              {isFetching && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              Load More
+            </Button>
+          }
         </>
       ) : (
         <NotConnected />
-      )}
-    </main>
+      )
+      }
+    </main >
   )
 }
